3c584e2204b64a8965ad79f7b4b6034830b5d561
Rahul Dey <jaswant@Jaswants-MacBook-Air.local>
2023-03-21 12:52:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />{" "}
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
